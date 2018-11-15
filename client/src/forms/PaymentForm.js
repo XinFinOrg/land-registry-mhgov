386551645a5ba53d381c61cd2168e1879f46832e
sauzy34 <sonugupta34@gmail.com>
2018-11-15 16:08:57 +0530

    financerPayment integrated

--- client/src/forms/PaymentForm.js
+++ client/src/forms/PaymentForm.js
@@ -9,32 +9,53 @@ import withRouter from 'react-router/withRouter'
 import { toast } from 'react-toastify'
 
 class PaymentForm extends Component {
-  state = { tokenAmtPaid: false }
+  state = { amtPaid: false }
 
   handlePay = async () => {
     const {
+      data,
       match: { params }
     } = this.props
-    try {
-      this.setState({ isLoading: true })
-      const { data } = await axios.post(`${API_URL}/payTokenAmount`, {
-        registryId: params.tab3
-      })
-      await this.setState({ isLoading: false, tokenAmtPaid: true })
-      await toast.success(`${'Token amount paid!'}`, {
-        position: toast.POSITION.TOP_CENTER
-      })
-      console.log('DATA', data)
-    } catch (error) {
-      await this.setState({ isLoading: false })
-      toast.error(`${'Error!!!'}`, {
-        position: toast.POSITION.TOP_CENTER
-      })
-      console.log('ERROR', error)
+    if (get(data, 'buyerFinancer.email', '') === Cookies.get('email') && data.status === 'registry_token_amount') {
+      try {
+        this.setState({ isLoading: true })
+        const { data } = await axios.post(`${API_URL}/financerPayment`, {
+          registryId: params.tab3
+        })
+        await this.setState({ isLoading: false, amtPaid: true })
+        await toast.success(`${'Financer amount paid!'}`, {
+          position: toast.POSITION.TOP_CENTER
+        })
+        console.log('DATA', data)
+      } catch (error) {
+        await this.setState({ isLoading: false })
+        toast.error(`${'Error!!!'}`, {
+          position: toast.POSITION.TOP_CENTER
+        })
+        console.log('ERROR', error)
+      }
+    } else {
+      try {
+        this.setState({ isLoading: true })
+        const { data } = await axios.post(`${API_URL}/payTokenAmount`, {
+          registryId: params.tab3
+        })
+        await this.setState({ isLoading: false, amtPaid: true })
+        await toast.success(`${'Token amount paid!'}`, {
+          position: toast.POSITION.TOP_CENTER
+        })
+        console.log('DATA', data)
+      } catch (error) {
+        await this.setState({ isLoading: false })
+        toast.error(`${'Error!!!'}`, {
+          position: toast.POSITION.TOP_CENTER
+        })
+        console.log('ERROR', error)
+      }
     }
   }
   render() {
-    const { isLoading, tokenAmtPaid } = this.state
+    const { isLoading, amtPaid } = this.state
     const { data } = this.props
     console.log('data==>', data)
     return (
@@ -44,8 +65,12 @@ class PaymentForm extends Component {
         shadow={'0px 2px 6.5px 0.5px rgba(0, 0, 0, 0.06)'}
         margin={'0 95px'}>
         <PaymentTuple>
-          <PaymentText>Token amount to be paid: {data.tokenAmt}</PaymentText>
-          {data.status === 'registry_token_amount' ? (
+          <PaymentText>
+            {get(data, 'buyerFinancer.email', '') === Cookies.get('email')
+              ? `Finance amount to be paid: ${get(data, 'buyerFinancer.financeAmount')}`
+              : `Token amount to be paid: ${data.tokenAmt}`}
+          </PaymentText>
+          {data.status === ('registry_token_amount' || 'registry_buyer_pay') || amtPaid ? (
             <StatusPage paid />
           ) : (
             <Button
