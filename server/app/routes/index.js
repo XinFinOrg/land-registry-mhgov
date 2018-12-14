61b12319fa689ac1f4c7041284ed9af6c673984e
atul rupnar <atulrupnar@gmail.com>
2018-12-14 19:09:38 +0530

    landRegistry : added stamp duty

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -472,7 +472,8 @@ router.post('/sellProperty', async function(req, res) {
 	    }
 
 	   	//console.log(data);
-	   	let stampDuty = (sellPrice * 5)/100 + 2000;
+	   	let stampDuty = (sellPrice * 5)/100;
+	   	stampDuty += (sellPrice/100 < 30000) ? 3000 : sellPrice/100;
 		query = {
 			registryId : registryId,
 			propertyId : propertyId,
